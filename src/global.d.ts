c1eb5ee5beb6d645f2cac805d196f0b200cb62f4
Domagoj Štrekelj <domagoj.strekelj@gmail.com>
2021-11-28 21:22:27 +0100

    feat: add global types for overloaded element

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,42 @@
+import React from "react";
+
 declare global {
   type AnyProps = Record<string, unknown>;
   type AnyComponent = (props: StyledProps) => JSX.Element;
+
+  // @see https://www.fullstacklabs.co/blog/overload-typescript-react-component-interfaces-prop-values
+
+  type DistributiveOmit<T, K extends keyof any> = T extends any
+    ? Omit<T, K>
+    : never;
+
+  type VoidElement =
+    | "area"
+    | "base"
+    | "br"
+    | "col"
+    | "hr"
+    | "img"
+    | "input"
+    | "link"
+    | "meta"
+    | "param"
+    | "command"
+    | "keygen"
+    | "source";
+
+  type OmitChildrenFromVoid<C extends React.ElementType> = C extends VoidElement
+    ? Omit<React.ComponentPropsWithRef<C>, "children">
+    : React.ComponentPropsWithRef<C>;
+
+  interface OverloadedElement<P> {
+    <C extends React.ElementType>(
+      props: { as: C } & P & DistributiveOmit<OmitChildrenFromVoid<C>, keyof P>
+    ): JSX.Element;
+    (
+      props: P & DistributiveOmit<React.ComponentPropsWithRef<"div">, keyof P>
+    ): JSX.Element;
+  }
 }
 
 export {};
